052d25939a3941bcc92bfeecf3b327a45f864a95
loicsrz <loic.serazin@insa-lyon.fr>
2019-04-08 15:07:12 -0400

    Delete debug message. Add comments

--- ExactMethod.cpp
+++ ExactMethod.cpp
@@ -33,7 +33,7 @@ bool checkCondition( int SupplierInventoryStock, int capacityVehicle, int choice
     {
         return false;
     }
-
+    /// Première Heuristique
     if(choice == 2 || choice == 4)
     {
         if(currentpath.visited_node.size() == 2)
@@ -48,7 +48,7 @@ bool checkCondition( int SupplierInventoryStock, int capacityVehicle, int choice
             return false;
         }
     }
-
+    /// Seconde Heuristique
     if(choice == 3 || choice == 4)
     {
         int somme=0;
@@ -81,10 +81,9 @@ vector<int> getNeighbours(path current, int number_retailer, int ** matrice)
 }
 
 /// Fonction permettant le calcul de la valeur de la fonction objective
-void objective_function (path *current, int * currentInventory, int number_retailer, int ** matrice, int * inventoryMax)
+void objective_function (path *current, int * currentInventory, int number_retailer, int ** matrice)
 {
     int tmpvalues [3];
-    //PrintPath(*current);
 
     int objective_number = 0;
 
@@ -99,23 +98,13 @@ void objective_function (path *current, int * currentInventory, int number_retai
     for (unsigned int j=0;j<current->visited_node.size()-1;j++)
     {
        objective_number += matrice[current->visited_node[j]][current->visited_node[j+1]];
-//       objective_number += inventoryMax[current->visited_node[j+1]] - currentInventory[current->visited_node[j+1]];
     }
     tmpvalues[1] = objective_number;
 
     /// Ajout de la valeur situé dans le camion et livré aux clients
-//    objective_number += inventoryMax[current->visited_node[current->visited_node.size()-1]] - currentInventory[current->visited_node[current->visited_node.size()-1]];
     current->objectif_value = objective_number;
 
     tmpvalues[2] = objective_number;
-//    if (objective_number >= 26220 && objective_number <= 26600)
-//    {
-//        PrintPath(*current);
-//        for (int i=0;i<3;i++)
-//        {
-//            cout << tmpvalues[i]<< endl;
-//        }
-//    }
 
 }
 /// Fonction récursive permettant de récupérer l'ensemble des chenins possibles
@@ -138,6 +127,7 @@ vector<path> recursiveFunction (path currentPath, int **  matrice, int * invento
         path NewcurrentPath = currentPath;
         NewcurrentPath.visited_node.push_back(neighbours[i]);
 
+        /// On met à jour la liste des noeuds critiques
         if(find(urgentNodes.begin(),urgentNodes.end(),neighbours[i]) != urgentNodes.end())
         {
             urgentNodes.erase(find(urgentNodes.begin(),urgentNodes.end(),neighbours[i]));
@@ -147,6 +137,8 @@ vector<path> recursiveFunction (path currentPath, int **  matrice, int * invento
         /// on met à jour l'entrepôt en fonction du nouveau noeud et on regarde si cela respecte les conditions
         int tempSupplier = SupplierInventoryStock - inventoryMax[neighbours[i]] + currentInventory[neighbours[i]];
         bool isValid = checkCondition( tempSupplier, capacityVehicle, choice, urgentNodes, currentPath, currentInventory, inventoryMax);
+
+        /// Si le path n'est pas valide et que le noeud ajouté était critique, on le remet avant de passer à la suite
         if(!isValid)
         {
             if(erase)
@@ -157,26 +149,27 @@ vector<path> recursiveFunction (path currentPath, int **  matrice, int * invento
         }
 
         /// On va récupérer de maniére récursive les paths fils
-        //objective_function(&NewcurrentPath, currentInventory, numberOfRetailer, matrice, inventoryMax);
         vector <path> NewTabPath;
         NewTabPath = recursiveFunction(NewcurrentPath, matrice, inventoryMax, currentInventory, numberOfRetailer, increaseSpeed, capacityVehicle, tempSupplier, choice, urgentNodes);
-        /// Une fois les paths récupérés, on vérifie qu'il vérifie les conditions
+
+        /// Permet de checker si le chemin parcours tous les noeuds critiques
         for (unsigned int k=0;k< NewTabPath.size();k++)
         {
             for(int j=1;j< numberOfRetailer;j++)
             {
+                /// Si un noeud critique est déja visité, on ne le prend pas en compte
                 if (find(NewTabPath[k].visited_node.begin(), NewTabPath[k].visited_node.end(), j) != NewTabPath[k].visited_node.end())
                 {
                     continue;
                 }
-
+                /// Si un noeud critique n'a pas été visité, on ne prend pas en compte ce chemin
                 if(currentInventory[j] + increaseSpeed[j] < 0)
                 {
                     PrintPath(NewTabPath[k]);
                     NewTabPath[k].objectif_value = -1;
                 }
             }
-
+            /// Si le chemin est bon, on le renvoie
             if (NewTabPath[k].objectif_value != -1)
             {
                 pathToGive.push_back(NewTabPath[k]);
@@ -184,20 +177,23 @@ vector<path> recursiveFunction (path currentPath, int **  matrice, int * invento
         }
     }
     currentPath.visited_node.push_back(0);
-    objective_function(&currentPath, currentInventory, numberOfRetailer, matrice, inventoryMax);
+    objective_function(&currentPath, currentInventory, numberOfRetailer, matrice);
+
+    /// Permet de checker si le chemin parcours tous les noeuds critiques
     for(int j=1;j< numberOfRetailer;j++)
     {
-        /// Permet de checker si le chemin parcours tous les noeuds critiques
+        /// Si un noeud critique est déja visité, on ne le prend pas en compte
         if (find(currentPath.visited_node.begin(), currentPath.visited_node.end(), j) != currentPath.visited_node.end())
         {
             continue;
         }
-
+        /// Si un noeud critique n'a pas été visité, on ne prend pas en compte ce chemin
         if(currentInventory[j] + increaseSpeed[j] < 0)
         {
             currentPath.objectif_value = -1;
         }
     }
+    /// Si le chemin est bon, on le renvoie
     if (currentPath.objectif_value != -1)
     {
         pathToGive.push_back(currentPath);
@@ -211,173 +207,60 @@ void creerInitialCondition (int ** MatriceAdjacence, int * inventoryMax, int * i
 {
     ///Création de la matrice et initialisation des différents paramètres du problème
     MatriceAdjacence[0][0] = INT8_MAX;
-    MatriceAdjacence[0][1] = 200;
-    MatriceAdjacence[0][2] = 200;
-    MatriceAdjacence[0][3] = 200;
-    MatriceAdjacence[0][4] = 200;
-    MatriceAdjacence[0][5] = 200;
-    MatriceAdjacence[0][6] = 200;
-    MatriceAdjacence[0][7] = 200;
-    MatriceAdjacence[0][8] = 200;
-    MatriceAdjacence[0][9] = 200;
-    MatriceAdjacence[0][10] = 200;
+    MatriceAdjacence[0][1] = 100;
+    MatriceAdjacence[0][2] = 100;
+    MatriceAdjacence[0][3] = 100;
+    MatriceAdjacence[0][4] = 100;
 
-    MatriceAdjacence[1][0] = 200;
+
+    MatriceAdjacence[1][0] = 100;
     MatriceAdjacence[1][1] = INT8_MAX;
-    MatriceAdjacence[1][2] = INT8_MAX;
+    MatriceAdjacence[1][2] = 10;
     MatriceAdjacence[1][3] = INT8_MAX;
     MatriceAdjacence[1][4] = INT8_MAX;
-    MatriceAdjacence[1][5] = INT8_MAX;
-    MatriceAdjacence[1][6] = 20;
-    MatriceAdjacence[1][7] = 20;
-    MatriceAdjacence[1][8] = 20;
-    MatriceAdjacence[1][9] = INT8_MAX;
-    MatriceAdjacence[1][10] =INT8_MAX;
 
-    MatriceAdjacence[2][0] = 200;
-    MatriceAdjacence[2][1] = INT8_MAX;
+
+    MatriceAdjacence[2][0] = 100;
+    MatriceAdjacence[2][1] = 10;
     MatriceAdjacence[2][2] = INT8_MAX;
-    MatriceAdjacence[2][3] = INT8_MAX;
+    MatriceAdjacence[2][3] = 10;
     MatriceAdjacence[2][4] = INT8_MAX;
-    MatriceAdjacence[2][5] = INT8_MAX;
-    MatriceAdjacence[2][6] = INT8_MAX;
-    MatriceAdjacence[2][7] = 20;
-    MatriceAdjacence[2][8] = INT8_MAX;
-    MatriceAdjacence[2][9] = 20;
-    MatriceAdjacence[2][10] =INT8_MAX;
 
-    MatriceAdjacence[3][0] = 200;
+
+    MatriceAdjacence[3][0] = 100;
     MatriceAdjacence[3][1] = INT8_MAX;
-    MatriceAdjacence[3][2] = INT8_MAX;
+    MatriceAdjacence[3][2] = 10;
     MatriceAdjacence[3][3] = INT8_MAX;
-    MatriceAdjacence[3][4] = INT8_MAX;
-    MatriceAdjacence[3][5] = INT8_MAX;
-    MatriceAdjacence[3][6] = INT8_MAX;
-    MatriceAdjacence[3][7] = INT8_MAX;
-    MatriceAdjacence[3][8] = INT8_MAX;
-    MatriceAdjacence[3][9] = 20;
-    MatriceAdjacence[3][10] =20;
+    MatriceAdjacence[3][4] = 10;
 
-    MatriceAdjacence[4][0] = 200;
+
+    MatriceAdjacence[4][0] = 100;
     MatriceAdjacence[4][1] = INT8_MAX;
     MatriceAdjacence[4][2] = INT8_MAX;
-    MatriceAdjacence[4][3] = INT8_MAX;
+    MatriceAdjacence[4][3] = 10;
     MatriceAdjacence[4][4] = INT8_MAX;
-    MatriceAdjacence[4][5] = 20;
-    MatriceAdjacence[4][6] = INT8_MAX;
-    MatriceAdjacence[4][7] = INT8_MAX;
-    MatriceAdjacence[4][8] = INT8_MAX;
-    MatriceAdjacence[4][9] = INT8_MAX;
-    MatriceAdjacence[4][10] =20;
-
-    MatriceAdjacence[5][0] = 200;
-    MatriceAdjacence[5][1] = INT8_MAX;
-    MatriceAdjacence[5][2] = INT8_MAX;
-    MatriceAdjacence[5][3] = INT8_MAX;
-    MatriceAdjacence[5][4] = 20;
-    MatriceAdjacence[5][5] = INT8_MAX;
-    MatriceAdjacence[5][6] = 20;
-    MatriceAdjacence[5][7] = INT8_MAX;
-    MatriceAdjacence[5][8] = 20;
-    MatriceAdjacence[5][9] = INT8_MAX;
-    MatriceAdjacence[5][10] =INT8_MAX;
-
-    MatriceAdjacence[6][0] = 200;
-    MatriceAdjacence[6][1] = 20;
-    MatriceAdjacence[6][2] = INT8_MAX;
-    MatriceAdjacence[6][3] = INT8_MAX;
-    MatriceAdjacence[6][4] = INT8_MAX;
-    MatriceAdjacence[6][5] = 20;
-    MatriceAdjacence[6][6] = INT8_MAX;
-    MatriceAdjacence[6][7] = INT8_MAX;
-    MatriceAdjacence[6][8] = 20;
-    MatriceAdjacence[6][9] = INT8_MAX;
-    MatriceAdjacence[6][10] = INT8_MAX;
-
-    MatriceAdjacence[7][0] = 200;
-    MatriceAdjacence[7][1] = 20;
-    MatriceAdjacence[7][2] = 20;
-    MatriceAdjacence[7][3] = INT8_MAX;
-    MatriceAdjacence[7][4] = INT8_MAX;
-    MatriceAdjacence[7][5] = INT8_MAX;
-    MatriceAdjacence[7][6] = INT8_MAX;
-    MatriceAdjacence[7][7] = INT8_MAX;
-    MatriceAdjacence[7][8] = 20;
-    MatriceAdjacence[7][9] = 20;
-    MatriceAdjacence[7][10] = INT8_MAX;
-
-    MatriceAdjacence[8][0] = 200;
-    MatriceAdjacence[8][1] = 20;
-    MatriceAdjacence[8][2] = INT8_MAX;
-    MatriceAdjacence[8][3] = INT8_MAX;
-    MatriceAdjacence[8][4] = INT8_MAX;
-    MatriceAdjacence[8][5] = 20;
-    MatriceAdjacence[8][6] = 20;
-    MatriceAdjacence[8][7] = 20;
-    MatriceAdjacence[8][8] = INT8_MAX;
-    MatriceAdjacence[8][9] = INT8_MAX;
-    MatriceAdjacence[8][10] =INT8_MAX;
-
-    MatriceAdjacence[9][0] = 200;
-    MatriceAdjacence[9][1] = INT8_MAX;
-    MatriceAdjacence[9][2] = 20;
-    MatriceAdjacence[9][3] = 20;
-    MatriceAdjacence[9][4] = INT8_MAX;
-    MatriceAdjacence[9][5] = INT8_MAX;
-    MatriceAdjacence[9][6] = INT8_MAX;
-    MatriceAdjacence[9][7] = 20;
-    MatriceAdjacence[9][8] = INT8_MAX;
-    MatriceAdjacence[9][9] = INT8_MAX;
-    MatriceAdjacence[9][10] =20;
-
-    MatriceAdjacence[10][0] = 200;
-    MatriceAdjacence[10][1] = INT8_MAX;
-    MatriceAdjacence[10][2] = INT8_MAX;
-    MatriceAdjacence[10][3] = 20;
-    MatriceAdjacence[10][4] = 20;
-    MatriceAdjacence[10][5] = INT8_MAX;
-    MatriceAdjacence[10][6] = INT8_MAX;
-    MatriceAdjacence[10][7] = INT8_MAX;
-    MatriceAdjacence[10][8] = INT8_MAX;
-    MatriceAdjacence[10][9] = 20;
-    MatriceAdjacence[10][10] =INT8_MAX;
-
-    inventoryMax[0] = 10000;
-    inventoryMax[1] = 1500;
-    inventoryMax[2] = 600;
-    inventoryMax[3] = 3000;
-    inventoryMax[4] = 4400;
-    inventoryMax[5] = 2600;
-    inventoryMax[6] = 3200;
-    inventoryMax[7] = 2000;
-    inventoryMax[8] = 4000;
-    inventoryMax[9] = 5000;
-    inventoryMax[10] = 2300;
-
-    increaseSpeed[0] = 10000;
-    increaseSpeed[1] = -500;
-    increaseSpeed[2] = -600;
-    increaseSpeed[3] = -800;
-    increaseSpeed[4] = -1300;
-    increaseSpeed[5] = -2000;
-    increaseSpeed[6] = -200;
-    increaseSpeed[7] = -2000;
-    increaseSpeed[8] = -500;
-    increaseSpeed[9] = -1000;
-    increaseSpeed[10] = -2300;
 
 
-    currentInventory[0] = 10000;
-    currentInventory[1] = 1500;
-    currentInventory[2] = 600;
-    currentInventory[3] = 3000;
-    currentInventory[4] = 4400;
-    currentInventory[5] = 2600;
-    currentInventory[6] = 3200;
-    currentInventory[7] = 2000;
-    currentInventory[8] = 4000;
-    currentInventory[9] = 5000;
-    currentInventory[10] = 2300;
+    inventoryMax[0] = 7000;
+    inventoryMax[1] = 5000;
+    inventoryMax[2] = 3000;
+    inventoryMax[3] = 2000;
+    inventoryMax[4] = 4000;
+
+
+    increaseSpeed[0] = 7000;
+    increaseSpeed[1] = -1000;
+    increaseSpeed[2] = -1000;
+    increaseSpeed[3] = -2000;
+    increaseSpeed[4] = -1500;
+
+
+
+    currentInventory[0] = 7000;
+    currentInventory[1] = 5000;
+    currentInventory[2] = 3000;
+    currentInventory[3] = 2000;
+    currentInventory[4] = 4000;
 
 
 }
@@ -385,12 +268,10 @@ void creerInitialCondition (int ** MatriceAdjacence, int * inventoryMax, int * i
 vector<path> min_onjective(vector<path> pastPath, int **  matrice, int * inventoryMax, int * currentInventory, int numberOfRetailer, int * increaseSpeed, int capacityVehicle, int SupplierInventoryStock, int choice)
 {
     vector<int> urgentNodes;
-    for (unsigned int i=0;i<numberOfRetailer;i++)
+    for ( int i=0;i<numberOfRetailer;i++)
     {
         currentInventory[i] = inventoryMax[i];
     }
-    //cout << "4.0 : " << currentInventory[4] << endl;
-    //cout << "pastPath : " << pastPath.size() << endl;
     /// Initialisation des paramètres en fonction du chemin précédent
     for (unsigned int l=0;l<pastPath.size();l++)
     {
@@ -409,14 +290,13 @@ vector<path> min_onjective(vector<path> pastPath, int **  matrice, int * invento
         }
         SupplierInventoryStock = currentInventory[0];
     }
-    for(unsigned int i=0;i<numberOfRetailer;i++)
+    for(int i=0;i<numberOfRetailer;i++)
     {
         if(currentInventory[i] + increaseSpeed[i] < 0)
         {
             urgentNodes.push_back(i);
         }
     }
-   // cout << "4.1 : " << currentInventory[4] << endl;
 
     /// Calcul des voisins du dernier noeud contenu dans le chemin
     vector<path> pathToGive;
@@ -439,21 +319,21 @@ vector<path> min_onjective(vector<path> pastPath, int **  matrice, int * invento
         }
 
         /// On va chercher récursivement tous les fils de ce chemin et on remonte tous ceux qui sont corrects
-        //objective_function(&NewcurrentPath, currentInventory, numberOfRetailer, matrice, inventoryMax);
         vector <path> NewTabPath;
         NewTabPath = recursiveFunction(NewcurrentPath, matrice, inventoryMax, currentInventory, numberOfRetailer, increaseSpeed, capacityVehicle, tempSupplier, choice, urgentNodes);
-        /// On regarde que le chemin une fois calculé respecte les conditions finales
+
+        /// Permet de checker si le chemin parcours tous les noeuds critiques
         for (unsigned int k=0;k< NewTabPath.size();k++)
         {
             for(int j=1;j< numberOfRetailer;j++)
             {
-                /// On vérifie qu'il n'y est pas de doublon
+                /// Si un noeud critique est déja visité, on ne le prend pas en compte
                if (find(NewTabPath[k].visited_node.begin(), NewTabPath[k].visited_node.end(), j) != NewTabPath[k].visited_node.end())
                 {
 
                     continue;
                 }
-                /// On regarde si aucun inventaire ne va se vider au tour prochain
+                /// Si un noeud critique n'a pas été visité, on ne prend pas en compte ce chemin
                 if(currentInventory[j] + increaseSpeed[j] < 0)
                 {
                     NewTabPath[k].objectif_value = -1;
@@ -466,8 +346,6 @@ vector<path> min_onjective(vector<path> pastPath, int **  matrice, int * invento
                 {
                     PrintPath(pastPath[1]);
                     PrintPath(NewTabPath[k]);
-//        cout << j << endl;
-//        cout << currentInventory[j] << endl;
                 }
                 pathToGive.push_back(NewTabPath[k]);
             }
@@ -481,6 +359,7 @@ vector<path> Recursive_temps (vector<path> tabPath, int length, vector<path> pre
 {
     vector<path> minTabPath;
     int minTabPathObjective =INT32_MAX;
+
     /// Si c'est le dernier tour, on renvoie.
     if(length == 0)
     {
@@ -495,16 +374,15 @@ vector<path> Recursive_temps (vector<path> tabPath, int length, vector<path> pre
         }
         if(tabPath.size() !=0)
         {
-           // cout << "Precendence 1 : " <<precedence.size() << endl;
             precedence.push_back(MinPath);
-           // cout << "Precendence 2 : " <<precedence.size() << endl;
         }
         return precedence;
     }
-
+    /// Pour tous les paths valides récupérés au niveau supérieur
     vector<path> newTabPath;
     for (unsigned int i=0;i<tabPath.size();i++)
     {
+        /// On push un chemin et on cherche tous les chemins possibles à partir de celui-ci
         precedence.push_back(tabPath[i]);
         newTabPath = min_onjective(precedence, matricePointeur, inventoryMax, currentInventory, numberOfNodes, increaseSpeed, capacityVehicle, SupplierInventoryStock, choice);
         if(newTabPath.size() == 0)
@@ -512,34 +390,23 @@ vector<path> Recursive_temps (vector<path> tabPath, int length, vector<path> pre
             precedence.pop_back();
             continue;
         }
-        //cout << "Precendece size : " << precedence.size()<<endl;
+        /// On lance le tour suivant
         vector<path> tabPrecedentPath = Recursive_temps(newTabPath, length-1,precedence,matricePointeur, inventoryMax, currentInventory, numberOfNodes, increaseSpeed, capacityVehicle, SupplierInventoryStock, choice);
         precedence.pop_back();
         int min_tab_optimum = 0;
+
+        /// On regarde si la liste de chemins contient la plus petite somme de fonction objective
         for (unsigned int k=0;k<tabPrecedentPath.size();k++)
        {
            min_tab_optimum += tabPrecedentPath[k].objectif_value;
        }
-     //  cout << "En cours de calcul :" << min_tab_optimum << endl;
        if (min_tab_optimum < minTabPathObjective && min_tab_optimum !=0)
        {
            minTabPath.clear();
-           //cout<<"NOUVEAU"<< endl;
 
            for(unsigned int j=0;j<tabPrecedentPath.size();j++)
            {
                 minTabPath.push_back(tabPrecedentPath[j]);
-//                if(j==2 )
-//                {
-//                    cout << "Length" << length << endl;
-//                    PrintPath(tabPrecedentPath[1]);
-//                    PrintPath(tabPrecedentPath[j]);
-//                    for (int l=0; l<numberOfNodes;l++)
-//                    {
-//                        cout << currentInventory[l] << endl;
-//                    }
-//                    cout << endl;
-//                }
 
            }
            minTabPathObjective = min_tab_optimum;
@@ -551,7 +418,7 @@ vector<path> Recursive_temps (vector<path> tabPath, int length, vector<path> pre
 int main() {
 
 
-    int numberOfNodes = 11;
+    int numberOfNodes = 5;
     int numberOfTimeRunning = -1;
 
     cout << "Veuillez rentrez un nombre de tour de livraison : " << endl;
@@ -606,8 +473,8 @@ int main() {
     delete currentInventory;
     delete increaseSpeed;
 
+    /// On affiche le temps d'execution
     time = clock() - time;
-
     cout << "Temps d'execution : " << ((float)time)/CLOCKS_PER_SEC;
 
     return 0;
